6ea8802493a05d83ddb5959d4a7b2ace3c7f14c7
plaul <lmor@kea.dk>
2022-02-01 11:47:45 +0100

    Initial commit

---
+++ src/main/java/sem3/fullstack/fullstack/repositories/PlayerRepository.java
@@ -0,0 +1,7 @@
+package sem3.fullstack.fullstack.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sem3.fullstack.fullstack.entity.Player;
+
+public interface PlayerRepository extends CrudRepository<Player,Integer> {}
+
